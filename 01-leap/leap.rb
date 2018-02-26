122a012ca0e01f790e958c64a24c00bd5a195ce0
Nora Peters <norapeters5@gmail.com>
2018-02-25 20:59:41 -0800

    leap.rb complete with all tests passing

---
+++ 01-leap/leap.rb
@@ -0,0 +1,14 @@
+
+def leap_year?(year)
+  year = year.to_i
+  if year % 4 == 0
+    if year % 100 == 0
+      if year % 400 == 0
+        return true
+      else
+        return false
+      end
+    end
+    return true
+  end
+end
