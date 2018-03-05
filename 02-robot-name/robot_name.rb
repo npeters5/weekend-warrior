665e1857d991551abcade0510b3976961335dcb0
Nora Peters <norapeters5@gmail.com>
2018-03-04 23:36:57 -0800

    robot_name complete

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,26 @@
+require "awesome_print"
+require "faker"
 
+class Robot
+
+  attr_accessor :name
+
+  def initialize
+    @name = create_name
+  end
+
+  def name
+    return @name
+  end
+
+  def create_name
+    [*('A'..'Z')].sample(2).join + Faker::Number.number(3)
+  end
+
+  def reset
+    @name = create_name
+  end
+
+end
+
+# puts Robot.new.name
