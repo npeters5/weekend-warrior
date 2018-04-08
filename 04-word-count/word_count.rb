96de4e09e856961536d3a5acc3f6eb991c21e02b
Nora Peters <norapeters5@gmail.com>
2018-04-07 20:07:00 -0700

    all tests passing

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
-
+def words(phrase)
+  words = phrase.split()
+  counts = {}
+  words.each do |word|
+    if counts.has_key?(word)
+      counts[word] += 1
+    else
+      counts.store(word, 1)
+    end
+  end
+  counts
+end
